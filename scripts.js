3b3d066ea1f9052d8e20a8948fe9928d2851bd8b
Chiru1710 <107366845+Chiru1710@users.noreply.github.com>
2025-01-08 10:42:31 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ const discordButton = document.getElementById('discord-button');
 const unlockedMessage = document.getElementById('unlocked-message');
 
 subscribeButton.addEventListener('click', () => {
-    window.open('https://www.youtube.com/channel/your-channel-id', '_blank');
+    window.open('https://youtube.com/@miyabhaiff646?si=e0blBjaHkcRY-kNz', '_blank');
     subscribeButton.classList.add('hidden');
     discordButton.classList.remove('hidden');
 });
 
 discordButton.addEventListener('click', () => {
-    window.open('https://discord.gg/your-server-id', '_blank');
+    window.open('https://discord.gg/TqjZsQ73', '_blank');
     discordButton.classList.add('hidden');
     unlockedMessage.classList.remove('hidden');
 });
